6a82e65e22f35d1fb6024611ef5ff701f00e6e23
Aleksei Viunik <vjunik88@gmail.com>
2023-01-29 02:01:03 +0300

    Some extra leetcode tasks were solved

--- leetcode_tasks/task_0001.py
+++ leetcode_tasks/task_0001.py
@@ -1,7 +1,9 @@
 """
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Given an array of integers nums and an integer target, return indices of the
+two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly one solution, and you may
+not use the same element twice.
 
 You can return the answer in any order.
 
@@ -13,14 +15,15 @@ Constraints:
 - Only one valid answer exists.
  
 
-Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+Follow-up: Can you come up with an algorithm that is less than O(n2) time
+complexity?
 """
 
-from typing import List
+from typing import Dict, List
 
 
 def twoSum(nums: List[int], target: int) -> List[int]:
-    nums_table = {}
+    nums_table: Dict[int: int] = {}
     for i in range(len(nums)):
         if target - nums[i] not in nums_table.keys():
             nums_table[nums[i]] = i
@@ -28,7 +31,7 @@ def twoSum(nums: List[int], target: int) -> List[int]:
             return [nums_table[target - nums[i]], i]
 
 
-#nums = [162, 2, 12, 36, 121, 59, 600, 188]
-#target = 95
-#
-#print(twoSum(nums, target))
+nums = [162, 2, 12, 36, 121, 59, 600, 188]
+target = 95
+
+print(twoSum(nums, target))
